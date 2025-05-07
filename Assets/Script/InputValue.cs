bff2698b3db1150f50f6b920456a22e36c3f584c
HyengjuK <rlagudwn2121@gmail.com>
2025-05-07 15:29:08 +0900

    HandAnimation Setting

---
+++ Assets/Script/InputValue.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputValue : MonoBehaviour
+{
+    public InputActionAsset inputAsset;
+    public Animator lAnim;
+    public Animator rAnim;
+
+    // 실행 시 한 번만 찾아오고 계속 재사용
+    private InputAction leftGrip;
+    private InputAction leftTrigger;
+    private InputAction rightGrip;
+    private InputAction rightTrigger;
+
+    void Start()
+    {
+        // 기존 인덱스 2: XRI LeftHand Interaction
+        var leftMap = inputAsset.FindActionMap("XRI Left Interaction");
+        leftGrip = leftMap.FindAction("Select Value");
+        leftTrigger = leftMap.FindAction("Activate Value");
+
+        // 기존 인덱스 5: XRI RightHand Interaction
+        var rightMap = inputAsset.FindActionMap("XRI Right Interaction");
+        rightGrip = rightMap.FindAction("Select Value");
+        rightTrigger = rightMap.FindAction("Activate Value");
+
+        // 활성화
+        leftGrip.Enable();
+        leftTrigger.Enable();
+        rightGrip.Enable();
+        rightTrigger.Enable();
+    }
+
+    void Update()
+    {
+        // 왼손 애니메이션 값 설정
+        float leftGripValue = leftGrip.ReadValue<float>();
+        float leftTriggerValue = leftTrigger.ReadValue<float>();
+        lAnim.SetFloat("Grip", leftGripValue);
+        lAnim.SetFloat("Trigger", leftTriggerValue);
+
+        // 오른손 애니메이션 값 설정
+        float rightGripValue = rightGrip.ReadValue<float>();
+        float rightTriggerValue = rightTrigger.ReadValue<float>();
+        rAnim.SetFloat("RightGrip", rightGripValue);
+        rAnim.SetFloat("RightTrigger", rightTriggerValue);
+    }
+}
